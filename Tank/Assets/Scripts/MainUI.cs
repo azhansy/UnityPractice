84484cd1b2506dc3779982f8b0eb6957ccaf57fc
shu <azhansy@hotmail.com>
2020-01-04 17:57:56 +0700

    添加不同场和动画

---
+++ Tank/Assets/Scripts/MainUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainUI : MonoBehaviour
+{
+
+    private int moveSpeed = 180;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("MainUII start====");
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("update=>" + transform.position.y);
+        if (transform.position.y < 447)
+        {
+            transform.Translate(new Vector3(0, 1, 0) * moveSpeed * Time.deltaTime, Space.World);
+        }
+      
+    }
+}
---
+++ Tank/Assets/Scripts/Option.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Option : MonoBehaviour
+{
+    private int choice = 1;
+    public GameObject pos1;
+    public GameObject pos2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position = pos1.transform.position;
+            choice = 1;
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position = pos2.transform.position;
+            choice = 2;
+
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
